51dcbab5dbfadd23d1da74604a38d4e8ad617882
yjxyuan <yjxyuan@hotmail.com>
2019-08-13 15:45:33 -0400

    add git test cases

---
+++ src/test/java/qaclickacademy/GitTestSample.java
@@ -0,0 +1,25 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void postjira()
+	{
+		System.out.println("Git Push");
+		
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("Git Into master");
+		
+	}
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("what do you think");		
+	}
+}
