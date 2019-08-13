ecee56d1506e0a6dba16e77b10c7204bd44976b4
yjxyuan <yjxyuan@hotmail.com>
2019-08-13 13:48:00 -0400

    First Commit

---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void postjira()
+	{
+		System.out.println("postjira");
+		
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+		
+	}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void ElementUI()
+	{
+		System.out.println("ElementUI");
+	}
+}
